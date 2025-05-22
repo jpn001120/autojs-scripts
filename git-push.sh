f34972400a627defd949c88309ad83aee8cb7149
jpn0011208 <jpn0011208@gmail.com>
2025-05-22 17:52:10 +0800

    自动提交: 2025-05-22 17:52:10

--- git-push.sh
+++ git-push.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# 检查并添加所有文件
-git add .
-
-# 提交修改，使用当前时间作为提交消息
-git commit -m "自动提交: $(date '+%Y-%m-%d %H:%M:%S')"
-
-# 拉取最新并变基整合
-git pull --rebase
-
-# 推送到当前分支
-git push origin $(git rev-parse --abbrev-ref HEAD)
+# 先判断是否有变更
+if ! git diff-index --quiet HEAD --; then
+    git add .
+    git commit -m "自动提交: $(date '+%Y-%m-%d %H:%M:%S')"
+    git pull --rebase
+    git push origin $(git rev-parse --abbrev-ref HEAD)
+else
+    echo "No changes to commit."
+fi
