3a1c218431c06a02c9771b8a4c58c7de12882013
jpn0011208 <jpn0011208@gmail.com>
2025-05-22 18:45:12 +0800

    自动提交: 2025-05-22 18:45:12

--- test-1.js
+++ test-1.js
@@ -153,8 +153,9 @@ function login() {
     click(471, 1358); sleep(800); // 粘贴邮箱
     // 获取根节点（UI 控件树的顶层）
     // 尝试查找任意可见节点作为起点
+    console.log("准备调用 selector().findOne()");
     let rootNode = selector().findOne(5000);  // 最多等待 5 秒
-
+    console.log("selector().findOne() 调用完毕");
     if (rootNode) {
         printUIInfo(rootNode);
     } else {
