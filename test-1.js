1648daa88035c6b4c8c3930a25451406c6fc4924
jpn0011208 <jpn0011208@gmail.com>
2025-05-23 11:44:09 +0800

    自动提交: 2025-05-23 11:44:09

--- test-1.js
+++ test-1.js
@@ -314,11 +314,38 @@ function getCode(shortid, maxAttempts = 30, interval = 5000) {
     toast("未获取到验证码");
     return null;
 }
+/**
+ * 点击指定文本控件的最近可点击父节点
+ * @param {string} textToFind - 控件上显示的文字
+ * @returns {boolean} 是否点击成功
+ */
+function clickNearestClickable(textToFind) {
+    let node = text(textToFind).findOne(3000); // 最多等3秒
+    if (!node) {
+        toast("未找到文本: " + textToFind);
+        return false;
+    }
 
+    // 向上找最近可点击的父节点
+    let clickableNode = node;
+    while (clickableNode && !clickableNode.clickable()) {
+        clickableNode = clickableNode.parent();
+    }
+
+    if (clickableNode) {
+        toast("找到可点击父节点，正在点击...");
+        return clickableNode.click();
+    } else {
+        // 如果没有可点击控件，则降级用坐标点击
+        toast("未找到 clickable 父节点，使用坐标点击");
+        let bounds = node.bounds();
+        return click(bounds.centerX(), bounds.centerY());
+    }
+}
 // 资料修改模块
 function editProfile() {
     // 1. 打开app 如果已经打开了就关闭再打开
-    
+
     // 2. 判断是否已经登录 如果没登录 就执行登录
 
     // 3. 点击profile 
