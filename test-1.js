5c4a31e4d0a0ab3e53d4c29d9ba4a2d5107c54e1
jpn0011208 <jpn0011208@gmail.com>
2025-05-23 16:07:20 +0800

    自动提交: 2025-05-23 16:07:20

--- test-1.js
+++ test-1.js
@@ -547,6 +547,7 @@ let currentState = STATES.CHECK_LAUNCH;
 
 function step() {
     switch (currentState) {
+        log('状态机判断中...')
         case STATES.CHECK_LAUNCH:
             log('状态: CHECK_LAUNCH');
             if (currentPackage() !== config.packageName) {
