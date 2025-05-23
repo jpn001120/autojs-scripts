e6404f080b0ed217df1c554b676dd5086161abc4
jpn0011208 <jpn0011208@gmail.com>
2025-05-23 17:20:05 +0800

    自动提交: 2025-05-23 17:20:05

--- test-1.js
+++ test-1.js
@@ -468,7 +468,7 @@ function editProfile() {
         log('开始下载头像图片');
         let path = "/sdcard/Pictures/avatar.png";  // 推荐路径
         files.createWithDirs(path); // 如果父目录不存在则创建
-        http.download(url, path)
+        http.download(config.features.editProfile.avatar, path)
         app.sendBroadcast(
             new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, android.net.Uri.parse("file://" + path))
         );
