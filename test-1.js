1f7a5cd045d24eef443fa4f3c439fbb83ce920f0
jpn0011208 <jpn0011208@gmail.com>
2025-05-26 18:05:18 +0800

    自动提交: 2025-05-26 18:05:18

--- test-1.js
+++ test-1.js
@@ -708,7 +708,7 @@ function uploadVideo() {
 
         // 2.点击 右下角相册按钮id('kno')
         log('id 选中左上角第一个图片');
-        id('kno').findOne().click();
+        id('id('bp2')').findOne().click();
         sleep(2000);
 
         // 3. 选择视频文件 id('h5l')
